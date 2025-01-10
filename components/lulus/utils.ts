18db4c56d51aa8babfa4b55d56b67070111511f3
Sueli Correa de Oliveira <oliver.sueli@gmail.com>
2025-01-10 17:51:07 -0300

    Translate month labels to Portuguese in utils file

--- components/lulus/utils.ts
+++ components/lulus/utils.ts
@@ -91,19 +91,19 @@ export function getIconeSigno(signo: string): string {
   }
 }
 
-export const months = [
-  { value: '01', label: 'January' },
-  { value: '02', label: 'February' },
-  { value: '03', label: 'March' },
-  { value: '04', label: 'April' },
-  { value: '05', label: 'May' },
-  { value: '06', label: 'June' },
-  { value: '07', label: 'July' },
-  { value: '08', label: 'August' },
-  { value: '09', label: 'September' },
-  { value: '10', label: 'October' },
-  { value: '11', label: 'November' },
-  { value: '12', label: 'December' },
+export const meses = [
+  { value: '01', label: 'Janeiro' },
+  { value: '02', label: 'Fevereiro' },
+  { value: '03', label: 'Março' },
+  { value: '04', label: 'Abril' },
+  { value: '05', label: 'Maio' },
+  { value: '06', label: 'Junho' },
+  { value: '07', label: 'Julho' },
+  { value: '08', label: 'Agosto' },
+  { value: '09', label: 'Setembro' },
+  { value: '10', label: 'Outubro' },
+  { value: '11', label: 'Novembro' },
+  { value: '12', label: 'Dezembro' },
 ];
 export const participants: Person[] = [
   {
